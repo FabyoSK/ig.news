21ef6ccb8c79f087b4556ec82921cd48d7c2da76
Fábio Alves <fabio.alves@chuva.io>
2021-09-28 18:35:31 -0100

    chore: add sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import styles from '../styles/home.module.scss'
+
 export default function Home() {
   return (
-    <h1>Hello FSK</h1>
+    <h1 className={styles.title}>
+      Hello
+      <span>
+        FSK
+      </span> 
+    </h1>
   )
 }
