f4dbc6a584c0bbfebb980252ff8669647213321a
Fábio Alves <fabio.alves@chuva.io>
2021-09-29 09:54:52 -0100

    Chore: add dynamic title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
+import Head from 'next/head'
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello
-      <span>
-        FSK
-      </span> 
-    </h1>
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>
+        Hello
+        <span>
+          FSK
+        </span> 
+      </h1>
+    </>
   )
 }
