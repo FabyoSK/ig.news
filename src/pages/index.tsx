e2fcd18bb8ca68c9d279b85598fa6b04a17e18b8
Fábio Alves <fabio.alves@chuva.io>
2021-09-29 10:52:26 -0100

    Chore: add header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,8 @@ export default function Home() {
       <Head>
         <title>Inicio | ig.news</title>
       </Head>
-      <h1>
-        Hello FSK
-      </h1>
+      <h1>Hello FSK</h1>
+
     </>
   )
 }
