9e2dd898b17f4a3732c642a53cee9b45ebec6320
Fábio Alves <fabio.alves@chuva.io>
2021-10-03 17:20:54 -0100

    feat: add initial prismic data on /post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,10 @@
 import { GetStaticProps } from 'next';
 import Head  from 'next/head';
+import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
 export default function Posts() {
   return (
@@ -37,9 +40,20 @@ export default function Posts() {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'post')
+  ], {
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100,
+  });
+
+  console.log(response);
+  
   return {
     props: {
-      posts,
+      // posts
     }
   }
 }
